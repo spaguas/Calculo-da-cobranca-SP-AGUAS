9df8f461a89d3e915b378acc30cc5a71b11f9697
Igor Correia <32550984+Economistico@users.noreply.github.com>
2026-08-14 13:29:49 -0300

    correção de quebra de linha

--- app.py
+++ app.py
@@ -792,8 +792,7 @@ CALCULADORAS_POR_BACIA = {
 
 col_esq, col_botao, col_dir = st.columns([3, 1, 3])
 with col_botao:
-    calcular = st.button("Calcular", type="primary",
-                         icon="🧮", use_container_width=True)
+    calcular = st.button("Calcular", type="primary", icon="🧮", use_container_width=True)
 
 if calcular:
     calculadora = CALCULADORAS_POR_BACIA.get(bacia_selecionada)
