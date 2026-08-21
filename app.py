0a6003e7c89f204c3d44be6f4b22d5477e9d7072
Igor Correia <32550984+Economistico@users.noreply.github.com>
2026-08-21 09:28:16 -0300

    quebra de linha

--- app.py
+++ app.py
@@ -514,8 +514,7 @@ def calcular_alto_paranapanema(tabela_captacao, tabela_lancamento):
     valor_lancamento_total = 0.0
 
     for _, linha in tabela_lancamento.iterrows():
-        volume_outorgado = linha["Vazão outorgada (m³/h)"] * \
-            linha["Horas/Dia"] * linha["Dias/Ano"]
+        volume_outorgado = linha["Vazão outorgada (m³/h)"] * linha["Horas/Dia"] * linha["Dias/Ano"]
         volume_medido = linha["Volume anual medido (m³)"]
 
         v_lanc = calcular_v_lanc(volume_outorgado, volume_medido)
